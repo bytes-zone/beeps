df352977d62e99bef09af730a2425f98c4e4bf3a
Brian Hicks <brian@brianthicks.com>
2024-10-04 12:35:37 -0500

    load data with fallback to default

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,57 @@
+use chrono::{DateTime, Utc};
 use clap::Parser;
-use color_eyre::Result;
+use color_eyre::{eyre, Result};
+use directories::ProjectDirs;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Document {
+    pings: Vec<Ping>,
+    lambda: f64,
+}
+
+impl Default for Document {
+    fn default() -> Self {
+        Self {
+            pings: Vec::new(),
+            lambda: 45.0 / 60.0,
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Ping {
+    time: DateTime<Utc>,
+    tag: Option<String>,
+}
 
 /// Keep track of what you're doing throughout the day by being annoyed by a robot.
 #[derive(Parser, Debug)]
 struct CLI {}
 
 impl CLI {
+    fn dirs(&self) -> Result<ProjectDirs> {
+        match ProjectDirs::from("zone", "bytes", "beeps") {
+            Some(l) => Ok(l),
+            None => Err(eyre::eyre!(
+                "Could not find a suitable location to store data"
+            )),
+        }
+    }
+
+    fn read(&self) -> Result<Document> {
+        let dirs = self.dirs()?;
+        let path = dirs.data_dir().join("data.json");
+        let data = std::fs::read_to_string(path)?;
+        let document = serde_json::from_str(&data)?;
+
+        Ok(document)
+    }
+
     fn run(&self) -> Result<()> {
-        println!("{self:#?}");
+        let loaded = self.read().unwrap_or_default();
+
+        println!("loaded: {loaded:#?}");
 
         Ok(())
     }
