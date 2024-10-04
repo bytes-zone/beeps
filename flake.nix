d8bb782718f51816dd9d40ceb6f2de025308e1bb
Brian Hicks <brian@brianthicks.com>
2024-10-04 06:30:11 -0500

    hello, world

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import inputs.nixpkgs { inherit system; };
+      in {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShell = pkgs.mkShell {
+          packages = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.cargo-edit
+            pkgs.rust-analyzer
+            pkgs.clippy
+            pkgs.rustfmt
+            pkgs.libiconv
+          ];
+        };
+      }
+    );
+}
