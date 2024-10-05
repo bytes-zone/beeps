0bb0eed7d8ea7497b1bbaf10b4c04a9ea737127b
Brian Hicks <brian@brianthicks.com>
2024-10-04 21:07:30 -0500

    implement data collection loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Local, Utc};
 use clap::Parser;
-use color_eyre::{eyre, Result};
+use color_eyre::{
+    eyre::{self, Context},
+    Result,
+};
 use directories::ProjectDirs;
 use rand_core::RngCore;
 use rand_pcg::Pcg32;
@@ -51,6 +54,18 @@ impl Document {
 
         Ok(())
     }
+
+    fn current_mut(&mut self) -> Option<&mut Ping> {
+        let now = Utc::now();
+
+        self.pings.iter_mut().rev().filter(|p| p.time <= now).next()
+    }
+
+    fn future(&self) -> Option<&Ping> {
+        let now = Utc::now();
+
+        self.pings.iter().rev().filter(|p| p.time > now).next()
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -85,18 +100,61 @@ impl CLI {
     fn read(&self) -> Result<Document> {
         let dirs = self.dirs()?;
         let path = dirs.data_dir().join("data.json");
-        let data = std::fs::read_to_string(path)?;
-        let document = serde_json::from_str(&data)?;
+        let data = std::fs::read_to_string(path).wrap_err("could not read data")?;
+        let document = serde_json::from_str(&data).wrap_err("could not deserialize data")?;
 
         Ok(document)
+    }
+
+    fn save(&self, document: &Document) -> Result<()> {
+        let dirs = self.dirs()?;
+        let path = dirs.data_dir().join("data.json");
+        let data = serde_json::to_string(document).wrap_err("could not serialize data")?;
+
+        std::fs::create_dir_all(dirs.data_dir()).wrap_err("could not create directory")?;
+        std::fs::write(path, data).wrap_err("could not write data")?;
+
+        Ok(())
     }
 
     fn run(&self) -> Result<()> {
         let mut loaded = self.read().unwrap_or_default();
 
-        loaded.fill()?;
+        loop {
+            loaded.fill().wrap_err("could not fill")?;
 
-        println!("loaded: {loaded:#?}");
+            if let Some(ping) = loaded.current_mut().filter(|p| p.tag.is_none()) {
+                println!(
+                    "What were you doing at {}?",
+                    ping.time.with_timezone(&Local).format("%-I:%M %p")
+                );
+                let mut tag = String::new();
+                std::io::stdin().read_line(&mut tag)?;
+                ping.tag = Some(tag.trim().to_string());
+            }
+
+            self.save(&loaded).wrap_err("could not save")?;
+
+            // fill again, just in case we waited forever to fill out the current ping
+            loaded.fill().wrap_err("could not fill")?;
+
+            if let Some(ping) = loaded.future() {
+                let now = Utc::now();
+                let duration = ping.time - now;
+                let duration = duration
+                    .to_std()
+                    .wrap_err("could not convert duration to std")?;
+                println!("waiting for next ping");
+                std::thread::sleep(duration);
+
+                std::process::Command::new("say")
+                    .arg("you have a new ping")
+                    .spawn()
+                    .wrap_err("could not invoke say")?;
+            } else {
+                break;
+            }
+        }
 
         Ok(())
     }
