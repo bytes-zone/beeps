fc518d0ddbf13f2b6f0961fff3675de5996c850e
Brian Hicks <brian@brianthicks.com>
2024-10-04 20:31:46 -0500

    fill pings

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use chrono::{DateTime, Utc};
 use clap::Parser;
 use color_eyre::{eyre, Result};
 use directories::ProjectDirs;
+use rand_core::RngCore;
+use rand_pcg::Pcg32;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -19,10 +21,51 @@ impl Default for Document {
     }
 }
 
+impl Document {
+    fn fill(&mut self) -> Result<()> {
+        if self.pings.is_empty() {
+            self.pings.push(Ping::default());
+        }
+
+        let now = Utc::now();
+        let mut current = self
+            .pings
+            .last()
+            .expect("there to be at least one ping after backfilling");
+
+        while current.time <= now {
+            let mut gen = Pcg32::new(current.time.timestamp().try_into()?, 0xa02bdbf7bb3c0a7);
+            let adjustment = (gen.next_u32() as f64 / u32::MAX as f64).ln() / self.lambda * -1.0;
+            let delta = chrono::Duration::minutes((adjustment * 60.0).floor() as i64);
+
+            let next = Ping {
+                time: current.time + delta,
+                tag: None,
+            };
+            self.pings.push(next);
+            current = self
+                .pings
+                .last()
+                .expect("there to be a last ping after pushing");
+        }
+
+        Ok(())
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 struct Ping {
     time: DateTime<Utc>,
     tag: Option<String>,
+}
+
+impl Default for Ping {
+    fn default() -> Self {
+        Self {
+            time: Utc::now(),
+            tag: None,
+        }
+    }
 }
 
 /// Keep track of what you're doing throughout the day by being annoyed by a robot.
@@ -49,7 +92,9 @@ impl CLI {
     }
 
     fn run(&self) -> Result<()> {
-        let loaded = self.read().unwrap_or_default();
+        let mut loaded = self.read().unwrap_or_default();
+
+        loaded.fill()?;
 
         println!("loaded: {loaded:#?}");
 
