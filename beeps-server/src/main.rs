339d6ce58ba55361e9761c7cf95018e1133ad7bf
Brian Hicks <brian@brianthicks.com>
2024-10-15 06:30:07 -0500

    mark authorization header as sensitive

--- beeps-server/src/main.rs
+++ beeps-server/src/main.rs
@@ -1,6 +1,8 @@
-use axum::{routing::get, Router};
+use std::iter::once;
+
+use axum::{http::header::AUTHORIZATION, routing::get, Router};
 use clap::Parser;
-use tower_http::{compression, limit, trace};
+use tower_http::{compression, limit, sensitive_headers, trace};
 use tracing::level_filters::LevelFilter;
 
 /// Keep track of what you're doing throughout the day by being annoyed by a robot.
@@ -26,6 +28,9 @@ async fn main() {
         .layer(trace::TraceLayer::new_for_http())
         .layer(compression::CompressionLayer::new())
         .layer(limit::RequestBodyLimitLayer::new(1024 * 1024 * 5))
+        .layer(sensitive_headers::SetSensitiveHeadersLayer::new(once(
+            AUTHORIZATION,
+        )))
         .route("/", get(|| async { "Hello, World!" }));
 
     tracing::info!(address = &options.address, "listening");
