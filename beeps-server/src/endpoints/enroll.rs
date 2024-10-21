b54cd4d76f6e5d7fdd26838ab4bfb67da18a68da
Brian Hicks <brian@brianthicks.com>
2024-10-21 08:44:47 -0500

    read the temporary header safely

--- beeps-server/src/endpoints/enroll.rs
+++ beeps-server/src/endpoints/enroll.rs
@@ -1,5 +1,20 @@
+use axum::http::{HeaderMap, StatusCode};
+
 use crate::conn::Conn;
 
-pub async fn handler(Conn(_): Conn) -> &'static str {
-    "OK"
+pub async fn handler(
+    Conn(_): Conn,
+    headers: HeaderMap,
+) -> Result<String, (StatusCode, &'static str)> {
+    let header = headers
+        .get("x-temp-account-id")
+        .ok_or((StatusCode::BAD_REQUEST, "X-Temp-Account-ID is required"))?;
+
+    match header.to_str() {
+        Ok(s) => Ok(s.to_string()),
+        Err(err) => {
+            tracing::warn!(?err, "could not convert header");
+            Err((StatusCode::BAD_REQUEST, "Could not read header"))
+        }
+    }
 }
