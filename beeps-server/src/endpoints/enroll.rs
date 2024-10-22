2539b6ddfa9235f1a17ee1d80a5ea89c66947991
Brian Hicks <brian@brianthicks.com>
2024-10-21 20:56:25 -0600

    do a query in enroll

--- beeps-server/src/endpoints/enroll.rs
+++ beeps-server/src/endpoints/enroll.rs
@@ -1,20 +1,37 @@
 use axum::http::{HeaderMap, StatusCode};
+use sqlx::query;
 
 use crate::conn::Conn;
 
+#[tracing::instrument]
 pub async fn handler(
-    Conn(_): Conn,
+    Conn(mut conn): Conn,
     headers: HeaderMap,
 ) -> Result<String, (StatusCode, &'static str)> {
     let header = headers
         .get("x-temp-account-id")
-        .ok_or((StatusCode::BAD_REQUEST, "X-Temp-Account-ID is required"))?;
+        .ok_or((StatusCode::BAD_REQUEST, "X-Temp-Account-ID is required"))
+        .and_then(|h| {
+            h.to_str().map_err(|err| {
+                tracing::warn!(?err, "could not convert header");
+                (StatusCode::BAD_REQUEST, "Could not read header")
+            })
+        })
+        .and_then(|s| {
+            s.parse::<i64>()
+                .map_err(|_| (StatusCode::BAD_REQUEST, "Needed an integer ID"))
+        })?;
 
-    match header.to_str() {
-        Ok(s) => Ok(s.to_string()),
-        Err(err) => {
-            tracing::warn!(?err, "could not convert header");
-            Err((StatusCode::BAD_REQUEST, "Could not read header"))
-        }
-    }
+    let aggregate = query!(
+        "SELECT MAX(node) FROM operations WHERE document_id = $1",
+        header
+    )
+    .fetch_one(&mut *conn)
+    .await
+    .map_err(|err| {
+        tracing::error!(?err, "error querying");
+        (StatusCode::INTERNAL_SERVER_ERROR, "error querying")
+    })?;
+
+    Ok(aggregate.max.unwrap_or(0).to_string())
 }
