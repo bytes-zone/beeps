e2b73e6f8034c700a7a9fe4cdeb615e573ddbbcd
Brian Hicks <brian@brianthicks.com>
2024-10-24 05:57:36 -0600

    add a way to require a jwt

---
+++ beeps-server/src/auth.rs
@@ -0,0 +1,63 @@
+use axum::extract::FromRef;
+use axum::http::request::Parts;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use axum::{async_trait, extract::FromRequestParts};
+use axum::{Json, RequestPartsExt};
+use axum_extra::headers::{authorization::Bearer, Authorization};
+use axum_extra::TypedHeader;
+use jsonwebtoken::{decode, DecodingKey, Validation};
+use serde::Deserialize;
+use serde_json::json;
+
+#[derive(Debug, Deserialize)]
+pub struct Claims {
+    sub: usize,
+    iat: usize,
+    exp: usize,
+}
+
+#[async_trait]
+impl<S> FromRequestParts<S> for Claims
+where
+    DecodingKey: FromRef<S>,
+    S: Send + Sync,
+{
+    type Rejection = AuthError;
+
+    async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let TypedHeader(Authorization(bearer)) = parts
+            .extract::<TypedHeader<Authorization<Bearer>>>()
+            .await
+            .map_err(|_| AuthError::InvalidToken)?;
+
+        let token_data = decode::<Claims>(
+            bearer.token(),
+            &DecodingKey::from_ref(state),
+            &Validation::default(),
+        )
+        .map_err(|err| {
+            tracing::trace!(?err, "error decoding token");
+            AuthError::InvalidToken
+        })?;
+
+        Ok(token_data.claims)
+    }
+}
+
+#[derive(Debug)]
+pub enum AuthError {
+    InvalidToken,
+}
+
+impl IntoResponse for AuthError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            AuthError::InvalidToken => (StatusCode::BAD_REQUEST, "Invalid token"),
+        };
+        let body = Json(json!({
+            "error": error_message,
+        }));
+        (status, body).into_response()
+    }
+}
--- beeps-server/src/main.rs
+++ beeps-server/src/main.rs
@@ -1,3 +1,4 @@
+mod auth;
 mod conn;
 mod endpoints;
 mod response;
