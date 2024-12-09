aa9e4050847ceb91fc817a99609fcda424d9dd5e
Brian Hicks <brian@brianthicks.com>
2024-12-09 07:10:49 -0600

    implement increment_at/increment/next

--- common/src/hlc.rs
+++ common/src/hlc.rs
@@ -1,7 +1,7 @@
 use chrono::{DateTime, Utc};
 use uuid::Uuid;
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Clone)]
 pub struct Hlc {
     timestamp: DateTime<Utc>,
     counter: u64,
@@ -15,6 +15,25 @@ impl Hlc {
             counter: 0,
             node,
         }
+    }
+
+    pub fn increment_at(&mut self, now: DateTime<Utc>) {
+        if now > self.timestamp {
+            self.timestamp = now;
+            self.counter = 0;
+        } else {
+            self.counter += 1;
+        }
+    }
+
+    pub fn increment(&mut self) {
+        self.increment_at(Utc::now());
+    }
+
+    pub fn next(&self) -> Self {
+        let mut next = self.clone();
+        next.increment();
+        next
     }
 }
 
@@ -36,10 +55,9 @@ impl PartialOrd for Hlc {
 #[cfg(test)]
 mod test {
     use super::*;
+    use chrono::Duration;
 
     mod ord {
-        use chrono::Duration;
-
         use super::*;
 
         #[test]
@@ -91,6 +109,50 @@ mod test {
                 node: Uuid::max(),
             };
             assert!(hlc1 < hlc2);
+        }
+    }
+
+    mod next {
+        use super::*;
+
+        #[test]
+        fn increments_counter_when_timestamp_is_in_the_past() {
+            let now = Utc::now();
+            let node = Uuid::new_v4();
+
+            let mut hlc = Hlc {
+                timestamp: now + Duration::seconds(1),
+                counter: 0,
+                node,
+            };
+            hlc.increment_at(now);
+
+            // changed
+            assert_eq!(hlc.counter, 1);
+
+            // unchanged
+            assert_eq!(hlc.timestamp, now + Duration::seconds(1));
+            assert_eq!(hlc.node, node);
+        }
+
+        #[test]
+        fn increments_timstamp_when_timestamp_is_in_the_future() {
+            let now = Utc::now();
+            let node = Uuid::new_v4();
+
+            let mut hlc = Hlc {
+                timestamp: now - Duration::seconds(1),
+                counter: 1,
+                node,
+            };
+            hlc.increment_at(now);
+
+            // changed
+            assert_eq!(hlc.timestamp, now);
+            assert_eq!(hlc.counter, 0);
+
+            // unchanged
+            assert_eq!(hlc.node, node);
         }
     }
 }
