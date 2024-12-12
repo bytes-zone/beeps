a5c62f54648fe410545e9ba1b4b43e73d3a21e2e
Brian Hicks <brian@brianthicks.com>
2024-12-12 13:42:38 -0600

    constraint seconds to within a couple years of now

--- common/src/test.rs
+++ common/src/test.rs
@@ -1,7 +1,7 @@
 use chrono::{DateTime, TimeZone, Utc};
 
 proptest::prop_compose! {
-    pub fn timestamp()(unix in 0..2_000_000_000_000i64) -> DateTime<Utc> {
+    pub fn timestamp()(unix in 1_700_000_000..1_800_000_000_000i64) -> DateTime<Utc> {
         Utc.timestamp_opt(unix, 0).unwrap()
     }
 }
