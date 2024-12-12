175b218e3dbd94b5b5d1bc339e02f4d7fb6b33e8
Brian Hicks <brian@brianthicks.com>
2024-12-12 07:19:43 -0600

    state should be mergeable

--- common/src/state.rs
+++ common/src/state.rs
@@ -1,6 +1,7 @@
 use crate::gmap::GMap;
 use crate::hlc::Hlc;
 use crate::lww::Lww;
+use crate::merge::Merge;
 use chrono::{DateTime, Utc};
 
 #[derive(serde::Serialize, serde::Deserialize)]
@@ -15,5 +16,14 @@ impl State {
             minutes_per_ping: Lww::new(45.0, clock.clone()),
             pings: GMap::new(),
         }
+    }
+}
+
+impl Merge for State {
+    fn merge(mut self, other: Self) -> Self {
+        self.minutes_per_ping = self.minutes_per_ping.merge(other.minutes_per_ping);
+        self.pings = self.pings.merge(other.pings);
+
+        self
     }
 }
