c02d78c745724e5710852fd63f632aa7f7eba652
Brian Hicks <brian@brianthicks.com>
2024-12-13 22:40:50 -0600

    do things with the value so the compiler doesn't drop it

--- browser/src/lib.rs
+++ browser/src/lib.rs
@@ -33,6 +33,11 @@ extern "C" {
 /// At the moment, this only references code we care about to make sure it's
 /// included in the bundle (and therefore can compile with wasm.) It's not
 /// intended to demonstrate any particular thing.
+///
+/// # Panics
+///
+/// Does some silly things to make sure all the code is included in the bundle
+/// so we can see how big we're getting.
 #[wasm_bindgen]
 pub fn main() {
     utils::set_panic_hook();
@@ -45,4 +50,15 @@ pub fn main() {
     let now = Utc::now();
     replica.add_ping(now);
     replica.tag_ping(now, "HI!".to_string());
+
+    alert(
+        replica
+            .state()
+            .pings
+            .get(&now)
+            .unwrap()
+            .value()
+            .as_ref()
+            .unwrap(),
+    );
 }
