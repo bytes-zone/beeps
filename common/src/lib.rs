6ae498cec790814b13b8313bed689dc0ca6f5cfd
Brian Hicks <brian@brianthicks.com>
2024-12-13 22:01:19 -0600

    add a few more useful lints

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,4 +1,10 @@
-#![warn(clippy::pedantic, clippy::allow_attributes, clippy::absolute_paths)]
+#![warn(
+    clippy::pedantic,
+    clippy::allow_attributes,
+    clippy::absolute_paths,
+    clippy::alloc_instead_of_core,
+    clippy::decimal_literal_representation
+)]
 #![allow(clippy::must_use_candidate)]
 
 pub mod gmap;
