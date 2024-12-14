1746a6034f658576351dfcf588434b8fff29f781
Brian Hicks <brian@brianthicks.com>
2024-12-13 22:37:39 -0600

    turn on pedantic mode in browser

--- browser/src/lib.rs
+++ browser/src/lib.rs
@@ -1,10 +1,27 @@
+#![warn(
+    missing_docs,
+    clippy::pedantic,
+    clippy::allow_attributes,
+    clippy::absolute_paths,
+    clippy::alloc_instead_of_core,
+    clippy::decimal_literal_representation,
+    clippy::missing_docs_in_private_items
+)]
+#![allow(clippy::must_use_candidate)]
+
+//! Browser interface for beeps storage, compiled to WASM.
+
+#[expect(clippy::missing_docs_in_private_items)]
 mod utils;
 
+use chrono::Utc;
 use common::node_id::NodeId;
+use common::replica::Replica;
 use wasm_bindgen::prelude::*;
 
 extern crate wee_alloc;
 
+/// Use the `wee_alloc` allocator for smaller WASM bundle size.
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
@@ -13,21 +30,19 @@ extern "C" {
     fn alert(s: &str);
 }
 
+/// At the moment, this only references code we care about to make sure it's
+/// included in the bundle (and therefore can compile with wasm.) It's not
+/// intended to demonstrate any particular thing.
 #[wasm_bindgen]
 pub fn main() {
     utils::set_panic_hook();
 
-    // At the moment, this only references code we care about to make sure it's
-    // included in the bundle (and therefore can compile with wasm.) It's not
-    // intended to demonstrate any particular thing.
     alert("Beginning test.");
 
-    let clock = common::hlc::Hlc::new(NodeId::random());
-    let lww = common::lww::Lww::new(1, clock);
+    let mut replica = Replica::new(NodeId::random());
+    replica.set_minutes_per_ping(45);
 
-    let mut map = common::gmap::GMap::new();
-    map.insert("test", lww);
-
-    alert("set value to…");
-    alert(&map.get(&"test").unwrap().value().to_string());
+    let now = Utc::now();
+    replica.add_ping(now);
+    replica.tag_ping(now, "HI!".to_string());
 }
