8f49ca976679ef5f7a68fd5d55dfa6d2d2fa58b4
Brian Hicks <brian@brianthicks.com>
2024-12-16 06:57:13 -0600

    wrap option

--- common/src/known.rs
+++ common/src/known.rs
@@ -3,26 +3,28 @@ use crate::merge::Merge;
 /// A CRDT that allows many replicas to register an unknown value without
 /// overwriting a known value. In practice, this behaves like a `Lww<Option<T>>`
 /// but it can go from `None` to `Some` and never back.
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 #[cfg_attr(test, derive(proptest_derive::Arbitrary))]
-pub enum Known<T: Merge> {
-    /// The value is unknown.
-    #[cfg_attr(test, proptest(weight = 1))]
-    Unknown,
+pub struct Known<T: Merge>(Option<T>);
 
-    /// The value is known.
-    #[cfg_attr(test, proptest(weight = 4))]
-    Known(T),
+impl<T: Merge> Known<T> {
+    pub fn unknown() -> Self {
+        Self(None)
+    }
+
+    pub fn known(value: T) -> Self {
+        Self(Some(value))
+    }
 }
 
 impl<T: Merge> Merge for Known<T> {
     fn merge(self, other: Self) -> Self {
-        match (self, other) {
-            (pick @ Self::Unknown, Self::Unknown) => pick,
-            (Self::Unknown, pick @ Self::Known(_)) => pick,
-            (pick @ Self::Known(_), Self::Unknown) => pick,
-            (Self::Known(self_k), Self::Known(other_k)) => Self::Known(self_k.merge(other_k)),
-        }
+        Self(match (self.0, other.0) {
+            (pick @ None, None) => pick,
+            (None, pick @ Some(_)) => pick,
+            (pick @ Some(_), None) => pick,
+            (Some(self_k), Some(other_k)) => Some(self_k.merge(other_k)),
+        })
     }
 }
 
@@ -38,14 +40,14 @@ mod test {
         proptest! {
             #[test]
             fn known_always_beats_unknown(other: Known<Lww<bool>>) {
-                assert_eq!(Known::Unknown.merge(other.clone()), other)
+                assert_eq!(Known::unknown().merge(other.clone()), other)
             }
 
             #[test]
             fn known_values_merge(a: Lww<bool>, b: Lww<bool>) {
                 assert_eq!(
-                    Known::Known(a.clone()).merge(Known::Known(b.clone())),
-                    Known::Known(a.merge(b))
+                    Known::known(a.clone()).merge(Known::known(b.clone())),
+                    Known::known(a.merge(b))
                 )
             }
 
