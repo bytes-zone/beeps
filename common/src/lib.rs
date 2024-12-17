59e30aa4fe98ad8297b1091f5ffa8e402a9fe5dc
Brian Hicks <brian@brianthicks.com>
2024-12-16 19:45:28 -0600

    do a bunch of re-exports

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -13,30 +13,38 @@
 
 /// A grow-only map (G-Map.) Values must be mergeable.
 pub mod gmap;
+pub use gmap::GMap;
 
 /// A grow-only set (G-Set.)
 pub mod gset;
+pub use gset::GSet;
 
 /// A Hybrid Logical clock (HLC)
 pub mod hlc;
+pub use hlc::Hlc;
 
 /// A Last-Write-Wins (LWW) register.
 pub mod lww;
+pub use lww::Lww;
 
 /// The interface all CRDTs must implement to merge.
 pub mod merge;
 
 /// A node ID.
 pub mod node_id;
+pub use node_id::NodeId;
 
 /// A replica (that is, state + node ID)
 pub mod replica;
+pub use replica::Replica;
 
 /// The state that gets synced between nodes.
 pub mod state;
+pub use state::State;
 
 /// Scheduling pings
 pub mod scheduler;
+pub use scheduler::Scheduler;
 
 #[cfg(test)]
 mod test;
