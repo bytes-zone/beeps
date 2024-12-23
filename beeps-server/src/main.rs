eec6eaa88b33cc092ff3d25e1e20552929b8cdd1
Brian Hicks <brian@brianthicks.com>
2024-12-23 07:22:50 -0600

    set up an axum server

--- beeps-server/src/main.rs
+++ beeps-server/src/main.rs
@@ -1,7 +1,11 @@
 //! A sync server for beeps.
 
+use axum::{http::header::AUTHORIZATION, response::IntoResponse, routing::get, Router};
 use clap::Parser;
-use std::time::Duration;
+use std::{iter::once, time::Duration};
+use tokio::net::TcpListener;
+use tower_http::{compression, decompression, limit, sensitive_headers, timeout, trace};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Debug, Parser)]
 struct Config {
@@ -31,5 +35,32 @@ fn duration_parser(s: &str) -> Result<Duration, std::num::ParseIntError> {
 async fn main() {
     let options = Config::parse();
 
-    println!("{:#?}", options);
+    // TODO: opentelemetry
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new()
+        .layer(trace::TraceLayer::new_for_http())
+        .layer(compression::CompressionLayer::new())
+        .layer(decompression::DecompressionLayer::new())
+        .layer(limit::RequestBodyLimitLayer::new(options.body_limit))
+        .layer(sensitive_headers::SetSensitiveHeadersLayer::new(once(
+            AUTHORIZATION,
+        )))
+        .layer(timeout::TimeoutLayer::new(options.request_timeout))
+        // ROUTES
+        .route("/", get(handler));
+    // STATE
+    // .with_state(state);
+
+    let listener = TcpListener::bind(options.address).await.unwrap();
+    tracing::info!(address = ?listener.local_addr(), "listening");
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> impl IntoResponse {
+    "Hello, World!"
 }
