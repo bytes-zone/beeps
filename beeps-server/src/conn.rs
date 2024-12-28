9a3188f075d2ef3562f1057786be635176010855
Brian Hicks <brian@brianthicks.com>
2024-12-28 13:49:17 -0600

    rename variable to be clearer

--- beeps-server/src/conn.rs
+++ beeps-server/src/conn.rs
@@ -9,14 +9,17 @@ use sqlx::{pool::PoolConnection, PgPool};
 pub struct Conn(pub PoolConnection<sqlx::Postgres>);
 
 #[async_trait]
-impl<S> FromRequestParts<S> for Conn
+impl<State> FromRequestParts<State> for Conn
 where
-    PgPool: FromRef<S>,
-    S: Send + Sync,
+    PgPool: FromRef<State>,
+    State: Send + Sync,
 {
     type Rejection = (StatusCode, String);
 
-    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+    async fn from_request_parts(
+        _parts: &mut Parts,
+        state: &State,
+    ) -> Result<Self, Self::Rejection> {
         let pool = PgPool::from_ref(state);
 
         let conn = pool
