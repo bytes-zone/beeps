652cca6a37ff3a5a3c546b34b6e6c1542e876a0f
Brian Hicks <brian@brianthicks.com>
2025-01-18 22:10:13 -0600

    verify that we don't have access

--- beeps-server/src/handlers/push.rs
+++ beeps-server/src/handlers/push.rs
@@ -1,7 +1,6 @@
 use crate::conn::Conn;
 use crate::error::Error;
 use crate::jwt::Claims;
-use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::Json;
 use beeps_core::sync::push;
@@ -10,8 +9,7 @@ use beeps_core::sync::push;
 pub async fn handler(
     Conn(mut conn): Conn,
     claims: Claims,
-    Path(document_id): Path<i64>,
-    Json(document): Json<push::Req>,
+    Json(req): Json<push::Req>,
 ) -> Result<Json<push::Resp>, Error> {
     // Validate that the user owns the document
     let authed_document = sqlx::query!(
@@ -19,16 +17,45 @@ pub async fn handler(
         JOIN accounts ON accounts.id = documents.owner_id \
         WHERE accounts.email = $1 AND documents.id = $2",
         claims.sub,
-        document_id,
+        req.document_id,
     )
     .fetch_optional(&mut *conn)
     .await?;
 
     bail_if!(
         authed_document.is_none(),
-        "No document with that ID under your account",
+        "Document not found",
         StatusCode::NOT_FOUND
     );
 
     Ok(Json(push::Resp {}))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::handlers::test::TestDoc;
+    use beeps_core::Document;
+    use sqlx::{pool::PoolConnection, Postgres};
+
+    #[test_log::test(sqlx::test)]
+    fn test_unknown_document_not_authorized(mut conn: PoolConnection<Postgres>) {
+        let doc = TestDoc::create(&mut conn).await;
+
+        let err = handler(
+            Conn(conn),
+            doc.claims(),
+            Json(push::Req {
+                document_id: doc.document_id + 1,
+                document: Document::default(),
+            }),
+        )
+        .await
+        .unwrap_err();
+
+        assert_eq!(
+            err.unwrap_custom(),
+            (StatusCode::NOT_FOUND, "Document not found".to_string())
+        )
+    }
 }
