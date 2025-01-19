d678c6c2b1552e1f2f0a65b9b19a7c0b011babb3
Brian Hicks <brian@brianthicks.com>
2025-01-18 22:32:53 -0600

    add tags migration

---
+++ beeps-server/migrations/20250119042817_tags.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS tags (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    document_id BIGINT NOT NULL,
+    -- value
+    ping TIMESTAMPTZ NOT NULL,
+    tag TEXT NOT NULL,
+    -- clock
+    clock TIMESTAMPTZ NOT NULL,
+    counter BIGINT NOT NULL,
+    node_id BIGINT NOT NULL,
+    -- references
+    FOREIGN KEY (document_id) REFERENCES accounts (id) ON DELETE CASCADE,
+    UNIQUE (document_id, clock, counter, node_id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_tags_document_id ON minutes_per_pings (document_id);
