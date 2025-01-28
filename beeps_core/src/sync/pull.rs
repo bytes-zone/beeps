3ceba63b873c2be2109c879b6e8a082f8a08480a
Brian Hicks <brian@brianthicks.com>
2025-01-27 19:30:16 -0600

    wrap the document in a struct to make adding fields later easier

--- beeps_core/src/sync/pull.rs
+++ beeps_core/src/sync/pull.rs
@@ -1,7 +1,12 @@
 use crate::Document;
+use serde::{Deserialize, Serialize};
 
 /// The current document, as seen by the server
-pub type Resp = Document;
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Resp {
+    /// The constructed document
+    pub document: Document,
+}
 
 /// Where the document push endpoint lives.
 pub static PATH: &str = "/api/v1/pull";
