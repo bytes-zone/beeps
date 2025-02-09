4a728cf5a1415605d980ec337d5d7b42b0e2aabf
Brian Hicks <brian@brianthicks.com>
2025-02-09 15:17:03 -0600

    try out a custom command

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -11,6 +11,12 @@ pub fn run() {
             }
             Ok(())
         })
+        .invoke_handler(tauri::generate_handler![my_custom_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn my_custom_command() {
+    println!("I was invoked from JS!")
 }
--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { invoke } from '@tauri-apps/api/core'
+
+const onClick = async () => {
+  await invoke('my_custom_command')
+}
+</script>
 
 <template>
   <p>This is a test!</p>
+
+  <button :onClick>Invoke</button>
 </template>
