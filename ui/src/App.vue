e0be8d16cc48974963da0a93578cf072e03c100b
Brian Hicks <brian@brianthicks.com>
2025-02-12 06:22:01 -0600

    extract the table to a component

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,20 +1,8 @@
 <script setup lang="ts">
 import { store } from './store'
+import TagTable from './components/TagTable.vue'
 </script>
 
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th>Ping</th>
-        <th>Tag</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="ping in store" :key="ping.ping.toString()">
-        <td>{{ ping.ping }}</td>
-        <td>{{ ping.tag }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <TagTable :pings="store" />
 </template>
---
+++ ui/src/components/TagTable.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { type Ping } from '../store.js'
+
+defineProps<{
+  pings: Ping[]
+}>()
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Ping</th>
+        <th>Tag</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="ping in pings" :key="ping.ping.toString()">
+        <td>{{ ping.ping }}</td>
+        <td>{{ ping.tag }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
