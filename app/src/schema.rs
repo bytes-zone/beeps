f6a5bfe67c98ed4f7c467e6e9f8be72d3906e53a
Brian Hicks <brian@brianthicks.com>
2025-02-15 21:58:16 -0600

    add tags table

--- app/src/schema.rs
+++ app/src/schema.rs
@@ -6,3 +6,19 @@ diesel::table! {
         ping -> Timestamp,
     }
 }
+
+diesel::table! {
+    tags (id) {
+        id -> Integer,
+        ping -> Timestamp,
+        tag -> Nullable<Text>,
+        timestamp -> Timestamp,
+        counter -> Integer,
+        node -> Integer,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    pings,
+    tags,
+);
