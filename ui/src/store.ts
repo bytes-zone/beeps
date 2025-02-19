569fbdb8a7450157fb19405b26ffbb8998fb48dc
Brian Hicks <brian@brianthicks.com>
2025-02-19 06:33:09 -0600

    clean up initialization code

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -3,10 +3,28 @@ import { commands, type PingWithTag } from './bindings'
 
 export const store = ref<PingWithTag[]>([])
 
+export const error = ref<string | null>(null)
+
+async function refreshDocument() {
+  store.value = (await commands.document()).pings
+}
+
+async function schedulePings() {
+  const result = await commands.schedulePings()
+
+  // TODO: this may not be in the right order
+  if (result.status == 'ok') {
+    store.value.unshift(...result.data)
+  } else {
+    error.value = `could not schedule new pings: ${result.error}`
+  }
+}
+
 // We use an IIFE because we want to maximize OS compatibility and Safari only
 // supports top-level await back to ~2021.
 ;(async () => {
-  await commands.document().then((doc) => {
-    store.value = doc.pings
-  })
+  setInterval(schedulePings, 10000)
+
+  await refreshDocument()
+  await schedulePings()
 })()
