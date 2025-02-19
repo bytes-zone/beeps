f58ce8129daf05fe4e9d5813d1b701c347d42716
Brian Hicks <brian@brianthicks.com>
2025-02-19 06:53:47 -0600

    dismiss the error on click

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import { current, error } from './store'
 import TagTable from './components/TagTable.vue'
+
+function dismissError() {
+  error.value = null
+}
 </script>
 
 <template>
-  <template v-if="error"> <span>Error:</span> {{ error }} </template>
+  <template v-if="error">
+    <span>Error:</span> {{ error }} <button @click="dismissError">X</button>
+  </template>
   <TagTable :pings="current" />
 </template>
