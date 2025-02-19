97ef72749eb77adae5d5c0b719ba989625594dcc
Brian Hicks <brian@brianthicks.com>
2025-02-19 06:52:35 -0600

    show error fi one exists

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { current } from './store'
+import { current, error } from './store'
 import TagTable from './components/TagTable.vue'
 </script>
 
 <template>
+  <template v-if="error"> <span>Error:</span> {{ error }} </template>
   <TagTable :pings="current" />
 </template>
